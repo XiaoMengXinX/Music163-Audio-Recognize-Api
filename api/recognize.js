0a7aef9985bf27214efab18301d7fc3cd2217a79
XiaoMengXinX <xibaole2333@gmail.com>
2022-07-15 00:05:31 +0800

    remove fs

--- api/recognize.js
+++ api/recognize.js
@@ -1,19 +1,37 @@
 const {Recognize} = require("../app");
-const formidable = require("formidable");
-const fs = require("fs");
+const querystring = require("querystring");
+const { v4: uuidv4 } = require("uuid");
+var buffer = ''
 
 module.exports = async (req, res) => {
-    const form = new formidable.IncomingForm();
-    form.parse(req, async (err, fields, files) => {
-        if (err || files.audio?.filepath === undefined) {
-            res.status(500).json({data: null, code: -1, message: "Parse form error"});
-            return;
-        }
-        let songData = fs.readFileSync(files.audio.filepath);
-        let result = await Recognize(songData);
-        if (result?.code !== 0) {
-            res.status(500)
-        }
-        return res.json(result);
+    let filename = uuidv4();
+
+    req.setEncoding("binary");
+    let body = ""; // 文件数据
+    let fileName = ""; // 文件名
+    const boundary = req.headers["content-type"]
+        .split("; ")[1]
+        .replace("boundary=", "");
+    req.on("data", function (chunk) {
+        body += chunk;
     });
+    req.on("end", function (filename) {
+        const file = querystring.parse(body, "\r\n", ":");
+        var fileInfo = file["Content-Disposition"].split("; ");
+        const entireData = body.toString();
+        contentType = file["Content-Type"].substring(1);
+        const upperBoundary = entireData.indexOf(contentType) + contentType.length;
+        const shorterData = entireData.substring(upperBoundary);
+        const binaryDataAlmost = shorterData
+            .replace(/^\s\s*/, "")
+            .replace(/\s\s*$/, "");
+        const binaryData = binaryDataAlmost.substring(0, binaryDataAlmost.indexOf("--" + boundary + "--"));
+        buffer = new Buffer.from(binaryData, "binary");
+    });
+
+    let result = await Recognize(buffer);
+    if (result?.code !== 0) {
+        res.status(500)
+    }
+    return res.json(result);
 }
